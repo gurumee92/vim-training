6681d4ce621b7690537ea973429cbed0612b215a
장현우 <gurumee92@gmail.com>
2023-01-02 21:03:25 +0900

    초기화

--- src/day15/ex02.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-int main(){
-    printf("Hello Mr. monkey\n");
-    printf("gurumee@naver.com\n");
-    return 0;
-}
--- src/day7/ex01.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main() {
-	printf("hello world");
-	printf("gurumee92@naver.com");
-	return 0;
-}
